e81f08b9fbaf6696240ae6ee9d569612db4dbf59
pmorris24 <patrickryanmorris23@gmail.com>
2025-08-11 22:12:44 -0400

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://takmfbrafitgeyfumdto.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRha21mYnJhZml0Z2V5ZnVtZHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NDEzMzQsImV4cCI6MjA3MDUxNzMzNH0.rDKJnLDDKQSxFRwb2du_nvvF2FJNrrZOPwrr51bbSdk';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
